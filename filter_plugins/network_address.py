9cc2f5920b930e5900d7f7d07f6a7796582dfaab
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-04-28 16:04:01 +0300

    Impoved ipv4_to_cidr filter (provide either netmask or prefix-length)

--- filter_plugins/network_address.py
+++ filter_plugins/network_address.py
@@ -5,22 +5,25 @@ class FilterModule(object):
     '''Provide additional network-address--related Jinja2 filters for Ansible'''
     
     @staticmethod
-    def ipv4_to_cidr(address, netmask):
+    def ipv4_to_cidr(address, netmask=None, prefixlen=None):
         a = IPAddress(address)
-        m = IPAddress(netmask)
-        assert m.is_netmask(), 'A valid netmask is required' 
         n = IPNetwork(a)
-        n.prefixlen = m.netmask_bits() 
+        if netmask:
+            assert prefixlen is None, 'Cannot provide both netmask and prefixlen'
+            m = IPAddress(netmask)
+            assert m.is_netmask(), 'A valid netmask is required' 
+            n.prefixlen = m.netmask_bits() 
+        else:
+            assert prefixlen, 'Provide either netmask or prefixlen'
+            n.prefixlen = int(prefixlen)
         return str(n.cidr)
 
     @staticmethod
-    def ipv4_inside_cidr(address, network):
-        a = IPAddress(address)
-        n = IPNetwork(network)
-        return (a in n)
+    def ipv4_in_cidr(address, network):
+        return (IPAddress(address) in IPNetwork(network))
 
     def filters(self):
         return {
            'ipv4_to_cidr': self.ipv4_to_cidr,
-           'ipv4_inside_cidr': self.ipv4_inside_cidr,
+           'ipv4_in_cidr': self.ipv4_in_cidr,
         }
