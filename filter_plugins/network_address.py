999db8b0e193d7c8f4190ca4843b333f3c01349e
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-04-28 13:56:57 +0300

    Rewrote network_address filters based on netaddr (Python) module

--- filter_plugins/network_address.py
+++ filter_plugins/network_address.py
@@ -1,15 +1,26 @@
+from netaddr import IPAddress, IPNetwork
+
 
 class FilterModule(object):
     '''Provide additional network-address--related Jinja2 filters for Ansible'''
     
     @staticmethod
     def ipv4_to_cidr(address, netmask):
-        assert netmask, 'A netmask is required' 
-        netmask_bits = sum([
-            bin(int(x)).count('1') for x in netmask.split('.')])
-        return '%s/%d' % (address, netmask_bits)
+        a = IPAddress(address)
+        m = IPAddress(netmask)
+        assert m.is_netmask(), 'A valid netmask is required' 
+        n = IPNetwork(a)
+        n.prefixlen = m.netmask_bits() 
+        return str(n.cidr)
+
+    @staticmethod
+    def ipv4_inside_cidr(address, network):
+        a = IPAddress(address)
+        n = IPNetwork(network)
+        return (a in n)
 
     def filters(self):
         return {
            'ipv4_to_cidr': self.ipv4_to_cidr,
+           'ipv4_inside_cidr': self.ipv4_inside_cidr,
         }
