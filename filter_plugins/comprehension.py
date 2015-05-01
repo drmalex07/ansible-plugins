a0ce262b8ca29129576861a8cf36f1c327035c75
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-01 22:44:32 +0300

    Improvements in filter plugins
    Fixed minor bug with `map_keys` filter (provide path-delimiter).
    Added `list_values` filter.

--- filter_plugins/comprehension.py
+++ filter_plugins/comprehension.py
@@ -6,22 +6,28 @@ class FilterModule(object):
     '''
     
     @staticmethod
-    def map_keys(d, keys, source_path=None):
+    def list_values(d):
+        '''List values from a dict'''
+        return d.values()
+    
+    @staticmethod
+    def map_keys(d, keys, source_path=None, path_delimiter='.'):
         '''Map keys on values from a set of (possibly nested) keys in an existing dict.
         
         If `source_path` is supplied, then it is expected to be a format string and 
         contain placeholder for the current key item, e.g.:
         
-        >>>  filter_by_keys(d, ['aa', 'bb']) # visits actual and explicitly-defined keys
-        >>>  filter_by_keys(d, ['aa', 'bb'], 'someprefix-{0}')   # visits actual keys 
-        >>>  filter_by_keys(d, ['aa', 'bb'], 'prefix-{0}-suffix.version')  # visits nested keys
+        >>>  map_keys(d, ['aa', 'bb']) # visits actual and explicitly-defined keys
+        >>>  map_keys(d, ['aa', 'bb'], 'someprefix-{0}')   # visits actual keys 
+        >>>  map_keys(d, ['aa', 'bb'], 'prefix-{0}-suffix.version')  # visits nested keys
         
         '''
         
         assert isinstance(keys, (list, set)), 'Expected an iterable'
         res = None
         if isinstance(source_path, str):
-            res = {k: reduce(lambda z,k: z.get(k), source_path.format(k).split('.'), d) 
+            f = lambda z,k: z.get(k) if z else None
+            res = {k: reduce(f, source_path.format(k).split(path_delimiter), d) 
                 for k in keys}
         else:
             res = {k: d.get(k) for k in keys}
@@ -30,4 +36,5 @@ class FilterModule(object):
     def filters(self):
         return {
            'map_keys': self.map_keys,
+           'list_values': self.list_values,
         }
