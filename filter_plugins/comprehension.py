95e08300e935eaaa22a3c4eb2a6581c8403587bf
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-06 01:25:24 +0300

    Added jinja2 filter: to_kv_pairs

--- filter_plugins/comprehension.py
+++ filter_plugins/comprehension.py
@@ -11,6 +11,18 @@ class FilterModule(object):
         return d.values()
     
     @staticmethod
+    def list_keys(d):
+        '''List keys from a dict'''
+        return d.keys()
+    
+    @staticmethod
+    def to_kv_pairs(d, sep=','):
+        '''Format a dict as KV pairs'''
+        f = lambda k, v: '{0}={1}'.format(k, v) if v else str(k)
+        return sep.join([f(k, v)
+            for k, v in d.items() if (not v or isinstance(v, basestring))])
+
+    @staticmethod
     def map_keys(d, keys, source_path=None, path_delimiter='.'):
         '''Map keys on values from a set of (possibly nested) keys in an existing dict.
         
@@ -37,4 +49,6 @@ class FilterModule(object):
         return {
            'map_keys': self.map_keys,
            'list_values': self.list_values,
+           'list_keys': self.list_keys,
+           'to_kv_pairs': self.to_kv_pairs,
         }
